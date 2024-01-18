3c03b08ffb03f4b69cc8b16f39f95b0d7b7e8c54
Lauryne Tranchant <laurynetranchant@mbpdejonathan2.home>
2024-01-18 11:29:45 +0100

    test code for remuneration before tax for eurl ir

---
+++ script.js
@@ -0,0 +1,36 @@
+import Engine, { formatValue } from "./node_modules/publicodes/dist/index.js";
+import rules from "./node_modules/modele-social/index.js";
+
+const engine = new Engine(rules);
+
+document.getElementById('calcul-btn').addEventListener('click', () => {
+    const turnover = parseFloat(document.getElementById('turnover').value);
+    const cost = parseFloat(document.getElementById('cost').value);
+    const situation = document.getElementById('select-situation').value;
+    const numberOfChild = parseInt(document.getElementById('child').value);
+    const householdIncome = parseFloat(document.getElementById('household-income').value);
+
+    const turnoverMinusCost = turnover - cost;
+
+    eurlSituation(turnoverMinusCost, cost, situation, numberOfChild, householdIncome, 'IS', 'non');
+});
+
+
+function eurlSituation(turnoverMinusCost, cost, situation, numberOfChild, householdIncome, tax, singleParent) {
+    engine.setSituation({
+        "dirigeant . rémunération . totale": turnoverMinusCost,
+        "impôt . foyer fiscal . situation de famille": `'${situation}'`,
+        "impôt . foyer fiscal . enfants à charge": parseInt(numberOfChild),
+        "impôt . foyer fiscal . revenu imposable . autres revenus imposables": parseFloat(householdIncome),
+        "entreprise . activité . nature": "'libérale'",
+        "entreprise . imposition": `'${tax}'`,
+        "entreprise . charges": cost,
+        "impôt . foyer fiscal . parent isolé": `${singleParent}`,
+        "entreprise . associés": "'unique'",
+        "entreprise . catégorie juridique": "'SARL'",
+        "impôt . méthode de calcul": "'barème standard'"
+    });
+
+    const net = engine.evaluate("dirigeant . rémunération . net");
+    document.querySelector('is-eurl-before').textContent = `${formatValue(net)}`;
+}
